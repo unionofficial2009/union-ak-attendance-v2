d8959b9232f52cb734af96852916b7ee68dfbca3
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-28 00:19:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 let cooldown = new Set();
-let cdseconds = 1;
+let cdseconds = 86400;
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
