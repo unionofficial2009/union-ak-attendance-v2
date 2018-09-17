e00d9f12ae34b059463bd15e8b98638e64f4586a
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-09-17 22:01:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,44 @@ bot.on("ready", async () => {
   
 });
 
+bot.on("message", async message => {
+  
+let prefix = botconfig.prefix;
+  
+let messageArray = message.content.split(" ");
+let cmd = messageArray[0];
+let args =  messageArray.slice(1);  
+
+ if(cmd === `${prefix}present`){
+   
+   let akmemberRole = message.guild.roles.find("name", "AK - Member");
+   
+   if(message.member.roles.has(akmemberRole.id)) {
+   
+    let c_user = message.author   
+    let bicon = c_user.displayAvatarURL;  
+    let bicon2 = bot.user.displayAvatarURL;
+   
+     let attendanceEmbed = new Discord.RichEmbed()
+    .setDescription(`${message.author}`)
+    .addField("Username", `${message.author.username}`)
+    .setColor("#15f153")
+    .setThumbnail(bicon)
+    .addField("Attendance", "Present")
+    .setTimestamp()
+    .setFooter("UNION AK Attendance",bicon2);
+     
+     let attendancechannel = message.guild.channels.find(`name`, "ak-attendance");
+     if (!attendancechannel) return message.channel.send("Couldn't find attendance channel.");
+     
+   } else {
+    message.reply(`${message.author.username}, you don't have the permission to use this command.`);    
+   }  
+   
+ }  
+  
+});
+
 
 
 bot.login(process.env.BOT_TOKEN);
