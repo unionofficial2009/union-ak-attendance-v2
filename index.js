c24491a696a8d759bb4c0b3358667a35ac71afa6
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-09-17 22:04:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const sql = require("sqlite");
+
 sql.open("./ak-attendance.sqlite");
-let cooldown = new Set();
-let cdseconds = 86400;
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
