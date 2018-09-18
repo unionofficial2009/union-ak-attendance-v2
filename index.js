3f511a1d4f5ebded651969413accf52020af23c7
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-09-18 13:58:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,23 +12,24 @@ bot.on("ready", async () => {
 });
 
 bot.on("message", async message => {
+	
 if (message.author.bot) return;
 if (message.channel.type !== "text") return;  
   
 let prefix = botconfig.prefix;
-  
 let messageArray = message.content.split(" ");
 let cmd = messageArray[0];
 let args =  messageArray.slice(1);  
-	
-	
+		
 if (cmd === `${prefix}create`) {
+	
    message.delete().catch(O_o=>{});
    sql.run('CREATE TABLE IF NOT EXISTS attendance (id TEXT, attendance_date,attendance_time)').then(row => {  
      message.reply(`Table attendance Successfully Created.`);	 
    }).catch((err) => {
      message.reply(`${err}`);	   
    });	   
+	
 } else if(cmd === `${prefix}present`){
 	
  let akmemberRole = message.guild.roles.find("name", "AK - Member");
@@ -36,8 +37,7 @@ if (cmd === `${prefix}create`) {
   if(message.member.roles.has(akmemberRole.id)) {
     let c_user = message.author   
     let bicon = c_user.displayAvatarURL;  
-    let bicon2 = bot.user.displayAvatarURL;
-	  
+    let bicon2 = bot.user.displayAvatarURL;	  
     let attendanceEmbed = new Discord.RichEmbed()
     .setDescription(`${message.author}`)
     .addField("Username", `${message.author.username}`)
