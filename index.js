245cd7477a1a3b9d6112827200ba508ebc7c2a82
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2019-01-30 16:55:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const sql = require("sqlite");
-
-sql.open("./ak-attendance.sqlite");
+let cooldown = new Set();
+let cdseconds = 86400;
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
@@ -12,72 +11,167 @@ bot.on("ready", async () => {
 });
 
 bot.on("message", async message => {
-	
-if (message.author.bot) return;
-if (message.channel.type !== "text") return;  
+//if(message.author.bot) return;
+if(message.channel.type === "dm") return;
   
 let prefix = botconfig.prefix;
+  
+if(!message.content.startsWith(prefix)) return;
 let messageArray = message.content.split(" ");
 let cmd = messageArray[0];
-let args =  messageArray.slice(1);  
-		
-if (cmd === `${prefix}create`) {
-	
-   message.delete().catch(O_o=>{});
-   sql.run('CREATE TABLE IF NOT EXISTS attendance (id TEXT, attendance_date,attendance_time)').then(row => {  
-     message.reply(`Table attendance Successfully Created.`);	 
-   }).catch((err) => {
-     message.reply(`${err}`);	   
-   });	   
-	
-} else if(cmd === `${prefix}present`){
-	
- let akmemberRole = message.guild.roles.find("name", "AK - Member");
-	
-  if(message.member.roles.has(akmemberRole.id)) {
-    let c_user = message.author   
-    let bicon = c_user.displayAvatarURL;  
-    let bicon2 = bot.user.displayAvatarURL;	  
-    let attendanceEmbed = new Discord.RichEmbed()
-    .setDescription(`${message.author}`)
-    .addField("Username", `${message.author.username}`)
-    .setColor("#15f153")
-    .setThumbnail(bicon)
-    .addField("Attendance", "Present")
-    .setTimestamp()
-    .setFooter("UNION AK Attendance",bicon2);
-	  
-     let attendancechannel = message.guild.channels.find(`name`, "ak-attendance");
-     if (!attendancechannel) return message.channel.send("Couldn't find attendance channel.");
-	  
+let args =  messageArray.slice(1);
+    
+ if(cmd === `${prefix}present`){
+   
+   if (message.channel.id != 540017296999972864 && message.author.id != 421634731709562886) {
      message.delete().catch(O_o=>{});
+     return message.reply("Invalid Channel! Please type ***present** here :arrow_right: <#540017296999972864>");
+   } 
+   
+   let today = new Date();
+   let newtoday = `${today.getMonth()+1}-${today.getDate()}-${today.getFullYear()}`;
+   let curHr = today.getHours();
+   let greetings = "";
+   let myattendance = 0;
+   
+   if (curHr < 12) {
+      greetings = 'Good Morning';
+   } else if (curHr < 18) {
+      greetings ='Good Afternoon';
+   } else {
+      greetings ='Good Evening';
+   }
+   
+   let attendancechannel = message.guild.channels.find(`name`, "sd-attendance");
+   if (!attendancechannel) return message.channel.send("Couldn't find attendance channel.");
+   
+   attendancechannel.fetchMessages({ limit: 100 })
+   .then(messages => {
      
-     let today_date = new Date().toLocaleDateString();
-     let today_time = new Date().toLocaleTimeString();
-	  
-     sql.get(`SELECT * FROM attendance WHERE id ="${message.author.id}"`).then(atn => {
-      if (!atn) {
-	
-        sql.run("INSERT INTO attendance (id, attendance_date,attendance_time) VALUES (?, ?, ?)", [message.author.id,today_date,today_time]);
-	attendancechannel.send(attendanceEmbed);
-	
+         
+      messages.forEach(function(messagecontent,messageid) {
+        
+
+        let userdate = `${messagecontent.createdAt.getMonth()+1}-${messagecontent.createdAt.getDate()}-${messagecontent.createdAt.getFullYear()}`;
+        
+         if(message.author.bot){
+        
+         if(messagecontent.embeds[0].fields[0].value == message.author.username && newtoday == userdate){
+          myattendance = myattendance + 1;
+         }  
+           
+         } else {  
+        
+        if(messagecontent.embeds[0].fields[0].value == message.member.displayName && newtoday == userdate){
+          myattendance = myattendance + 1;
+        }  
+       
+        }   
+           
+     })  
+     
+     if (myattendance > 0){
+       
+        if(message.author.bot){
+          
+          message.reply("You already have attendance for today.")  
+             
+        } else {
+          
+         let romemberRole = message.guild.roles.find("name", "SD - Member"); 
+       
+        if(message.member.roles.has(romemberRole.id)) {
+           message.reply("You already have attendance for today.")  
+        } else {
+          message.reply("You don't have the permission to use this command."); 
+        }
+          
+          
+        }  
+       
+           
+     }  else {
+       if(message.author.bot){
+         
+         
+      message.reply(`${greetings} ${message.author.username}  :tada::hugging: !`);
+     
+      let c_user = message.author   
+      let bicon = c_user.displayAvatarURL;   
+      let bicon2 = bot.user.displayAvatarURL; 
+     
+     let attendanceEmbed = new Discord.RichEmbed()
+     .setDescription(`${message.author}`)
+     .addField("Display Name", `${message.author.username}`)
+     .addField("Username", `${message.author.username}`)
+     .addField("Tag", `${message.author.tag}`)
+     .addField("ID", `${message.author.id}`)
+     .setColor("#15f153")
+     .setThumbnail(bicon)
+     .addField("Attendance", "Present")
+     .setTimestamp()
+     .setFooter("UNION SD Attendance",bicon2);
+     
+      let attendancechannel = message.guild.channels.find(`name`, "sd-attendance");
+      if (!attendancechannel) return message.channel.send("Couldn't find attendance channel.");
+      
+      attendancechannel.send(attendanceEmbed); 
+                  
+       } else {
+         
+      let romemberRole = message.guild.roles.find("name", "SD - Member");  
+      if(message.member.roles.has(romemberRole.id)) {
+      
+      message.reply(`${greetings} ${message.member.displayName}  :tada::hugging: !`);  
+        
+     let c_user = message.author   
+     let bicon = c_user.displayAvatarURL;   
+     let bicon2 = bot.user.displayAvatarURL;   
+     
+     let attendanceEmbed = new Discord.RichEmbed()
+     .setDescription(`${message.author}`)
+     .addField("Display Name", `${message.member.displayName}`)
+     .addField("Username", `${message.author.username}`)
+     .addField("Tag", `${message.author.tag}`)
+     .addField("ID", `${message.author.id}`)
+     .setColor("#15f153")
+     .setThumbnail(bicon)
+     .addField("Attendance", "Present")
+     .setTimestamp()
+     .setFooter("UNION SD Attendance",bicon2);
+     
+     let attendancechannel = message.guild.channels.find(`name`, "sd-attendance");
+     if (!attendancechannel) return message.channel.send("Couldn't find attendance channel.");   
+        
+     attendancechannel.send(attendanceEmbed);   
+        
       } else {
-        message.reply(`You already have attendance for today.`);
-	message.channel.send( `user:${atn.id} \n attendance_date:${atn.attendance_date} \n attendance_time:${atn.attendance_time}`);
-      }
-      
-    }).catch((err) => {
-      message.reply(`${err}`);
-      
-   });
-	  
-  } else {
-    message.reply(`you don't have the permission to use this command.`);    
-  } 	 	
- 
- }
-                                    
+      message.reply("You don't have the permission to use this command.");
+          
+      }           
+         
+      }  
+       
+     }  
+       
+    message.delete().catch(O_o=>{});   
+   }).catch(console.error);
   
+  }
+  
+  if(cmd === `${prefix}botinfo`){
+   
+  let bicon = bot.user.displayAvatarURL;
+  let botembed = new Discord.RichEmbed()
+  .setDescription("Bot Information")
+  .setColor("#15f153")
+  .setThumbnail(bicon)
+  .addField("Bot Name", bot.user.username);
+   
+  return message.channel.send(botembed);
+}
+    
 });
+
 
 bot.login(process.env.BOT_TOKEN);
